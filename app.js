6ac5a2a20d96449339a2180d9d600db9f428e152
Adhamfm <57398840+Adhamfm@users.noreply.github.com>
2023-07-24 23:07:15 +0300

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json 	renamed:    views/404.html -> views/404.ejs 	renamed:    views/about.html -> views/about.ejs 	new file:   views/create.ejs 	new file:   views/index.ejs 	deleted:    views/index.html 	new file:   views/partials/footer.ejs 	new file:   views/partials/head.ejs 	new file:   views/partials/nav.ejs

--- app.js
+++ app.js
@@ -4,27 +4,35 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 // Listen for requests
 app.listen(3000);
+
+const blogs = [
+    { title: 'first', snippet: "adhasdf asdfhajksd adfjhn asldkfjhasd asdufihsad" },
+    { title: 'second', snippet: "qertqweyrdfhety ueujhn asldkfjhaeuetusd asdufihsad" },
+];
 
 app.get('/', (req, res) => {
     //res.send()
     console.log(req.url, req.method);
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Parrot Blog', blogs });
 });
 
 app.get('/about', (req, res) => {
     console.log(req.url, req.method);
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About Adhamfm' });
 });
 
 // redirects
-app.get('/about-us', (req, res) => {
+app.get('/blogs/create', (req, res) => {
     console.log(req.url, req.method);
-    res.redirect('/about');
+    res.render('create', { title: 'Create Blogs' });
 });
 
 // 404 page (Needs to be after the last requests, )
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.render('404', { title: '404 - Page Not Found | 404' });
 });
